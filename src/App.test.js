5afb61d0e0660b25c8b946d979900e01655285e2
Cheesie <115614743+Cheesie11@users.noreply.github.com>
2025-11-13 15:19:16 +0100

    Updated Test to match UI Msg

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders react app msg', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/react app/i);
   expect(linkElement).toBeInTheDocument();
 });
